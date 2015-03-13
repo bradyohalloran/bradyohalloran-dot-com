2d3cdd73ba8c59e828ecd405d5cf8db1f1012673
Brady O'Halloran <bradyohalloran@gmail.com>
2015-03-13 14:42:26 +1100

    More gulp scripting done. Server and live reload complete. Build directory appears to be cleaning in the proper sequence. Will have to keep an eye on it.

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,23 @@ var jslint = require('gulp-jslint');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var slim = require('gulp-slim');
+var minifyCSS = require('gulp-minify-css');
+var connect = require('gulp-connect');
+var del = require('del');
 
-gulp.task('default', function() {
-	// task goes here
-})
+var livereload = require('gulp-livereload');
 
-gulp.task('sass', function() {
-	gulp.src('./sass/main.sass')
+gulp.task('default', ['connect', 'watch']);
+
+gulp.task('sass', ['clean'], function() {
+	gulp.src('src/sass/main.sass')
 		.pipe(sass({indentedSyntax: true}))
 		.pipe(sourcemaps.write())
+		.pipe(minifyCSS({keepBreaks:true}))
 		.pipe(gulp.dest('./build/css'));
 });
 
-gulp.task('js', function() {
+gulp.task('js', ['clean'], function() {
 	gulp.src(['lib/modernizr/modernizr.js', 'lib/jquery/dist/jquery.min.js'])
 		.pipe(concat('main.js'))
 		.pipe(uglify())
@@ -25,11 +29,32 @@ gulp.task('js', function() {
 		.pipe(gulp.dest('build/js'))
 });
 
-gulp.task('slim', function() {
-	gulp.src("slim/*.slim")
+gulp.task('slim', ['clean'], function() {
+	gulp.src('src/slim/*.slim')
 		.pipe(slim({
 			pretty: true
 		}))
 		.pipe(gulp.dest("build/"))
+		.pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+	gulp.watch(['./src/**/*.*'], ['build']);
 })
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'build',
+		livereload: true
+	});
+})
+
+gulp.task('clean', function() {
+	console.log('Cleaning build folder...');
+	del('build/**/*.*');
+})
+
+gulp.task('build', ['slim', 'sass', 'js']);
+
+
 
